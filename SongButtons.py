02da210b8bc06ebc7b268731f39352160b673f89
Tori Williams <vwilliams@guilford.edu>
2023-11-08 14:00:58 -0500

    added (almost) working next and back buttons

--- SongButtons.py
+++ SongButtons.py
@@ -1,7 +1,7 @@
 import os
 import pygame
 from pygame import mixer
-import time
+import keyboard
 
 # Initialize the mixer
 mixer.init()
@@ -21,32 +21,49 @@ def switcher(argument):
         7: "Its Beginning to Look a Lot Like Christmas.wav",
         8: "Jingle Bell Rock.wav",
         9: "Last Christmas.wav",
-        10: "Santa Claus Is Coming to Town.wav",
+        10: "Santa Claus Is Comin to Town.wav",
         11: "Santa Tell Me.wav",
         12: "Underneath the Tree.wav",
     }
-    return switcher.get(argument, "Invalid song number")
+    return switcher.get(argument, None)
 
-# Give mp3 song name with keyboard input using the switcher function
+# Give wav song name with keyboard input using the switcher function
 song_number = int(input("Enter a song number: "))
 song_name = switcher(song_number)
 print(song_name)
-
-def play_song(song_name):
-    # Load the song from the switcher function
-    song_path = "Christmas Music WAV/" + song_name
+        
+# Play the initial song
+if song_name is not None:
+    song_path = os.path.join(music_folder_path, song_name)
     mixer.music.load(song_path)
     mixer.music.play()
-
-# Play the song until the user presses the stop button
-play_song(song_name)
+    
 while True:
-    print("Press 's' to stop the song")
-    stop = input()
-    if stop == "s":
+    # if right arrow key is pressed, increment song number by 1
+    if keyboard.is_pressed('right arrow'):
+        song_number += 1
+        song_name = switcher(song_number)
+        if song_number > 12:
+            song_number = 1
+        if song_name is not None:
+            print(song_name)
+            song_path = os.path.join(music_folder_path, song_name)
+            mixer.music.load(song_path)
+            mixer.music.play()
+    # if left arrow key is pressed, decrement song number by 1
+    elif keyboard.is_pressed('left arrow'):
+        song_number -= 1
+        if song_number < 1:
+            song_number = 12
+        song_name = switcher(song_number)
+        if song_name is not None:
+            print(song_name)
+            song_path = os.path.join(music_folder_path, song_name)
+            mixer.music.load(song_path)
+            mixer.music.play()
+    # if s is pressed, stop the song
+    elif keyboard.is_pressed('s'):
         mixer.music.stop()
         break
     else:
-        print("Invalid input")
         continue
-    
