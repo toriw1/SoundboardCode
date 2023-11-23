751f5e79454433e5508a2c19d954d03a66e01346
Tori Williams <vwilliams@guilford.edu>
2023-11-22 23:07:36 -0500

    combined songbuttons/gui bc its simple & effective

--- Driver.py
+++ Driver.py
@@ -1,7 +1,9 @@
-import SongButtons
+import tkinter as tk
+from SongGUI import SongGUI  # Adjust the import based on your file structure
 
 def main():
-    SongButtons.start_program()
+    gui = SongGUI()
+    gui.mainloop()
 
 if __name__ == "__main__":
     main()
--- SongGUI.py
+++ SongGUI.py
@@ -1,31 +1,103 @@
-from tkinter import Tk, Label, Frame, LEFT, RIGHT
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+import pygame
+from pynput import keyboard
 
-class SongGUI:
+class SongGUI(tk.Tk):
     def __init__(self):
-        self.root = Tk()
-        self.root.title("Christmas Music Player")
-        self.root.geometry("1024x600")
-        self.root.configure(bg="green")
+        super().__init__()
 
-        # Create a frame to split the GUI
-        frame_left = Frame(self.root, bg="green")
-        frame_left.pack(side=LEFT, padx=20)
+        self.paused = False
+        self.song_number = 1
+        pygame.mixer.init()
 
-        frame_right = Frame(self.root, bg="green")
-        frame_right.pack(side=RIGHT, padx=20)
+        # Get the absolute path of the script's directory
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Create a label to display song information
-        self.song_info_label = Label(frame_right, text="Now Playing: None", font=("Helvetica", 12), bg="green", fg="white", anchor="e")
-        self.song_info_label.pack(pady=20)
+        # Use the absolute path to construct the relative path to the image
+        initial_image_path = os.path.join(script_dir, "GUIPictures/ChristmasBackground.png")
 
-    def update_song_info(self, song_name):
-        # Update the song information label
-        self.song_info_label.config(text=f"Now Playing: {song_name}")
+        self.image_path = initial_image_path
+        self.load_image()
 
-    def start_gui(self):
-        # Run the Tkinter main loop
-        self.root.mainloop()
+        self.panel = tk.Label(self, image=self.image)
+        self.panel.image = self.image
+        self.panel.pack()
+
+        self.bind('<Key>', self.on_press)
+
+    def load_image(self):
+        image = Image.open(self.image_path)
+        image = image.resize((1024, 600))
+        self.image = ImageTk.PhotoImage(image)
+
+    def update_image(self, new_image_path):
+        self.image_path = new_image_path
+        self.load_image()
+        self.panel.configure(image=self.image)
+        self.panel.image = self.image
+
+    def switcher(self, argument):
+        switcher = {
+            1: ("All I Want for Christmas Is You.wav", "GUIPictures/All I Want for Christmas Is You.png"),
+            2: ("Blue Christmas.wav", "GUIPictures/Blue Christmas.png"),
+            3: ("Christmas (Baby Please Come Home).wav", "GUIPictures/Christmas (Baby Please Come Home).png"),
+            4: ("Feliz Navidad.wav", "GUIPictures/Feliz Navidad.png"),
+            5: ("Here Comes Santa Claus.wav", "GUIPictures/Here Comes Santa Claus.png"),
+            6: ("Holly Jolly Christmas.wav", "GUIPictures/Holly Jolly Christmas.png"),
+            7: ("Ill Be Home for Christmas.wav", "GUIPictures/Ill Be Home for Christmas.png"),
+            8: ("Its Beginning to Look a Lot Like Christmas.wav", "GUIPictures/Its Beginning to Look a Lot Like Christmas.png"),
+            9: ("Jingle Bell Rock.wav", "GUIPictures/Jingle Bell Rock.png"),
+            10: ("Last Christmas.wav", "GUIPictures/Last Christmas.png"),
+            11: ("Santa Tell Me.wav", "GUIPictures/Santa Tell Me.png"),
+            12: ("Underneath the Tree.wav", "GUIPictures/Underneath the Tree.png"),
+        }
+        return switcher.get(argument, (None, None))
+
+    def notify(self, *args, **kwargs):
+        song_number = args[0]
+        self.song_number = song_number
+        self.play_song()
+
+    def play_song(self):
+        try:
+            song_name, image_path = self.switcher(self.song_number)
+            if song_name is not None:
+                song_path = os.path.join("Christmas Music WAV", song_name)
+                pygame.mixer.music.load(song_path)
+                pygame.mixer.music.play()
+
+                # Print the song information after playing
+                print(self.song_number, song_name)
+
+                # Update the GUI
+                self.update_image(image_path)
+        except pygame.error as e:
+            print(f"An error occurred while trying to play the song: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+    def on_press(self, event):
+        if event.keysym == 'Right':
+            self.song_number = (self.song_number % 12) + 1
+        elif event.keysym == 'Left':
+            self.song_number = (self.song_number - 2) % 12 + 1
+
+        # Use a dictionary to handle numeric key presses
+        numeric_keys = {
+            str(i): i for i in range(1, 13)
+        }
+
+        if event.keysym in numeric_keys:
+            self.song_number = numeric_keys[event.keysym]
+
+        if event.keysym == 's':
+            pygame.mixer.music.stop()
+
+        # Play the song
+        self.play_song()
 
 if __name__ == "__main__":
-    song_gui = SongGUI()
-    song_gui.start_gui()
+    gui = SongGUI()
+    gui.mainloop()
